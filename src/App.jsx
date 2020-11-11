23a910595e1fd2b582ba894e96c4a7c6e1709652
Navan Sami <navan.sami@yahoo.com>
2020-11-11 20:18:37 +0200

    added accordion component and styling library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is front end javascript framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favourite JS library amongst engineers'
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating components'
+  }
+]
 
 const App = () => {
   return (
     <div>
-      App
+      <Accordion items={items} />
     </div>
   );
 }
