ff6cc60d0893d94f8e848980985a22c92a900200
Navan Sami <navan.sami@yahoo.com>
2020-11-12 04:31:51 +0200

    fixed xss issue

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -31,10 +31,8 @@ const Search = () => {
     return(
       <div key={result.pageid} className="item" >
         <div className="content" >
-          <div className="header" >
-            { result.title }
-          </div>
-            { result.snippet }
+          <div className="header" > { result.title } </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     )
