ef08b07135ec98a810b0d6907b3c56ca5757585a
Navan Sami <navan.sami@yahoo.com>
2020-11-12 04:28:23 +0200

    loaded axios and made an api result to wikipedia

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  //State Assign
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+
+  //Watch Changes on Term
+  useEffect(() => {
+    const search = async () => {
+      const res = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+      setResults(res.data.query.search);
+    }
+
+    if(term) search();
+  },[term]);
+
+
+  //Render out the results
+  const renderedResults = results.map(result => {
+    return(
+      <div key={result.pageid} className="item" >
+        <div className="content" >
+          <div className="header" >
+            { result.title }
+          </div>
+            { result.snippet }
+        </div>
+      </div>
+    )
+  });
+
 
   return(
     <div>
@@ -14,6 +52,9 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        { renderedResults }
       </div>
     </div>
   )
