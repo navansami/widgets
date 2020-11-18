d344fe8f1c26dcbdb46e3aa7235a3c96cfd34858
Navan Sami <navan.sami@yahoo.com>
2020-11-18 13:23:28 +0200

    added popstate event to detect location

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,6 +4,9 @@ const Link = ({ className, href, children }) => {
   const onClicked = (event) => {
     event.preventDefault();
     window.history.pushState({},'', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   }
 
   return <a onClick={onClicked} href={ href } className={ className } > { children } </a>
