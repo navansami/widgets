cc2a560e0f68e15e13ef4992448fc973c87ef60d
Navan Sami <navan.sami@yahoo.com>
2020-11-18 08:52:40 +0200

    removed selected item from options

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 const DropDown = ({ options, selected, onSelectedChange }) => {
   const renderedOptins = options.map((option) => {
+    if(option.value === selected.value) {
+      return null;
+    }
     return (
       <div 
         key={option.value}
