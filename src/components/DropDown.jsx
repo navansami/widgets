cb66a0351cbc400d896d17e1e280a37ebed78fa7
Navan Sami <navan.sami@yahoo.com>
2020-11-18 09:03:40 +0200

    added ternary option to visible and active classes

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 const DropDown = ({ options, selected, onSelectedChange }) => {
-  const renderedOptins = options.map((option) => {
+  
+  const [open, setOpen] = useState(false);
+
+  const renderedOptions = options.map((option) => {
     if(option.value === selected.value) {
       return null;
     }
@@ -19,11 +22,13 @@ const DropDown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form ">
       <div className="field" >
         <label className="label" >Select a color</label>
-        <div className="ui selection dropdown visible active" >
+        <div
+          onClick={() => setOpen(!open) } 
+          className={`ui selection dropdown ${open ? 'visible active': ''}`} >
           <i className="dropdown icon"></i>
           <div className="text"> { selected.label } </div>
-          <div className="menu visible transition">
-            { renderedOptins }
+          <div className={`menu ${open ? 'visible transition' : ''} `} >
+            { renderedOptions }
           </div>
         </div>
       </div>
