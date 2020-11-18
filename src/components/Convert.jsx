747a18b27d7f7714335d2a93fbd489abfcdce643
Navan Sami <navan.sami@yahoo.com>
2020-11-18 12:20:33 +0200

    added debounced useEffect to limit number of api requests

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -2,19 +2,41 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-  
+  const [translated, setTranslated] = useState('');
+  const [debouncedTranslated, setDebouncedTranslated] = useState(text);
 
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-      params: {
-        q: text,
-        target: language.value,
-        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-      }
-    });
-  }, [language, text])
+    const timeId = setTimeout(() => {
+      setDebouncedTranslated(text)
+    }, 400);
 
-  return <div />
+    return () => {
+      clearTimeout(timeId);
+    }
+  }, [text]);
+
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data }  = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: debouncedTranslated,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      });
+      setTranslated(data.data.translations[0].translatedText);
+    } 
+
+    doTranslation();
+
+  }, [language, debouncedTranslated])
+
+  return (
+    <div>
+      <h1 className="ui header" > { translated } </h1>
+    </div>
+  );
 }
 
 export default Convert;
